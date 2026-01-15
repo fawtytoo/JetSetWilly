5729eb8a37667aee33ab2d28dba4c3bee2764ef8
fawtytoo <fawtytoo@gmx.co.uk>
2026-01-15 15:40:25 +0000

    Video blurry at lower resolutions

--- src/misc.c
+++ src/misc.c
@@ -61,9 +61,9 @@ int Video_Viewport(int width, int height, int *x, int *y, int *w, int *h)
     *y = (height - *h) / 2;
 
     multiply = *h / HEIGHT;
-    if (multiply > 2)
+    if (multiply < 1)
     {
-        multiply -= 2;
+        multiply = 1;
     }
 
     return multiply;
